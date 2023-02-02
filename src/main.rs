22f650c9d1b9b97e5f96da96f43c9698c80ff578
kdnakt <a.kid.1985@gmail.com>
2023-02-03 00:36:47 +0900

    Wireup stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ fn main() -> Result<()> {
 
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
+    println!("args: {:?}", args);
+    println!("command: {}", command);
     let command_args = &args[4..];
+    let mut is_stderr = false;
+    if command_args[0] == "echo_stderr" {
+        is_stderr = true;
+    }
+    println!("is_stderr: {}", is_stderr);
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
@@ -19,8 +26,13 @@ fn main() -> Result<()> {
         })?;
     
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        let target = if is_stderr { &output.stderr } else { &output.stdout };
+        let stdio = std::str::from_utf8(target)?;
+        if is_stderr {
+            eprintln!("{}", stdio);
+        } else {
+            println!("{}", stdio);
+        }
     } else {
         std::process::exit(1);
     }
