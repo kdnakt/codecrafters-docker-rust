003e130dbf59c3b202ec59c7ebafbebd57172619
kdnakt <a.kid.1985@gmail.com>
2023-02-03 00:41:55 +0900

    remove debug log

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ use anyhow::{Context, Result};
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
-    println!("args: {:?}", args);
-    println!("command: {}", command);
+    // println!("args: {:?}", args);
+    // println!("command: {}", command);
     let command_args = &args[4..];
     let mut is_stderr = false;
     if command_args[0] == "echo_stderr" {
         is_stderr = true;
     }
-    println!("is_stderr: {}", is_stderr);
+    // println!("is_stderr: {}", is_stderr);
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
